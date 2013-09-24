df9b71f72996464514f925ad443dbbe88cf5d098
Forrest Alvarez <forrest.alvarez@gmail.com>
2013-09-24 05:51:25 +0000

    updated tests to remove the output files.

--- test/test_flip_text.py
+++ test/test_flip_text.py
@@ -2,24 +2,27 @@
 
 import unittest
 from flipText import flipText
+import os
 
 class testFileParsing(unittest.TestCase):
 
 	def test_normal_file(self):
-		flipText('test1.txt')
+		flipText('test1.txt', 'test1out.txt')
 		with open('test1out.txt') as f:
 			fixed_text = f.readlines()
-		self.assertEqual('asdf', fixed_text)
+		self.assertEqual("['John Mills\tare you\table?\n', 'biz\tbar\tfoo\n']", fixed_text)
+		os.remove('test1out.txt')
 
-	def test_white_space(self):
-		flipText('test2.txt')
+	def test_blank_lines(self):
+		flipText('test2.txt', 'test2out.txt')
 		with open('test2out.txt') as f:
 			fixed_text = f.readlines()
-		self.assertEqual('asdf', fixed_text)
+		self.assertEqual("['\n', '\n', 'John Mills\tare you\table?\n', '\n', '\n', 'biz\tbar\tfoo\n', '\n', '\n']", fixed_text)
+		os.remove('test2out.txt')				
 
-	def test_weird_chars(self):
-		flipText('test3.txt')
+	def test_double_tabs(self):
+		flipText('test3.txt', 'test2out.txt')
 		with open('test1out.txt') as f:
 			fixed_text = f.readlines()
-		self.assertEqual('asdf', fixed_text)
-
+		self.assertEqual("['John Mills\tare you\t\table?\n', 'biz\t\tbar\tfoo\n', '\n', '\n']", fixed_text)
+		os.remove('test3out.txt')
