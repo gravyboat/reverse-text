fc297b49c9908dbd27023a54e114b6b3cf17eb0b
Forrest Alvarez <forrest.alvarez@gmail.com>
2013-09-25 02:19:11 +0000

    removed a few lines from flipText.py that I was using for testing

--- flipText/flipText.py
+++ flipText/flipText.py
@@ -6,6 +6,3 @@ def flipText(text_input_file, text_output_file):
 		final_list = '\t'.join(words[::-1])
 		with open(text_output_file, "a") as f:
 			f.write(final_list + "\n")
-
-if __name__ == "__main__":
-	flipText('input.txt', 'output.txt')
